fc388b8626b7b065cf33da3eaf449bb3aeaa5769
Danil <ddexpo.thebad@gmail.com>
2026-01-04 20:41:22 +0600

    2015 DONE Yahooooo~~~

--- AOC_2015_Python/19_Medicine_For_Rudolph/2_Medicine_For_Rudolph.py
+++ AOC_2015_Python/19_Medicine_For_Rudolph/2_Medicine_For_Rudolph.py
@@ -1,16 +1,14 @@
-from collections import defaultdict
+import re
 from pathlib import Path
-from pprint import pprint
+from collections import defaultdict
 
 with open(Path(__file__).resolve().parent / "task_data", "r") as file:
 
     possible_replacements = defaultdict(list)
     medicine_molecule     = ""
-    answ = set()
-    
+
     for line in file.readlines():
         line = line.strip()
-
         if len(line) > 100 or not line:
             medicine_molecule = line.strip()
             continue
@@ -18,5 +16,14 @@ with open(Path(__file__).resolve().parent / "task_data", "r") as file:
         molecules = line.split("=>")
         possible_replacements[molecules[0].strip()].append(molecules[1].strip())
 
-    pprint(possible_replacements)
-    print(len(medicine_molecule))
+    # EBATI
+    # ChatGPt solution is too good
+    elements = re.findall(r"[A-Z][a-z]?", medicine_molecule)
+    steps = (
+        len(elements)
+        - medicine_molecule.count("Rn")
+        - medicine_molecule.count("Ar")
+        - 2 * medicine_molecule.count("Y")
+        - 1
+    )
+    print(steps)
